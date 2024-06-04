bca0c1b7d975dedf7ac62cd54efa3f72a90034d1
Dylan Sarikas <97060448+dylansarikas@users.noreply.github.com>
2024-06-04 16:57:24 +0000

    adds rock moves in views

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -5,6 +5,18 @@ class MovesController < ApplicationController
   end
 
   def rock
+    moves = ["rock", "paper", "scissors"]
+
+    @computer = moves.sample
+
+    if @computer == "paper"
+      @output = "lost"
+    elsif @computer == "scissors"
+      @output = "won"
+    else
+      @output = "tied"
+    end
+    
     render({:template => "game_templates/rock_moves"})
   end
 
